f4ca6d0fd675d15c805428560fdfff6d7afd49eb
Peter1613 <reachpjok@gmail.com>
2023-10-02 00:03:26 -0400

    properly coded restaurant_data.py 10/1/23

--- restaurant_data.py
+++ restaurant_data.py
@@ -4,26 +4,25 @@
 '''
 
 restaurant_1 = {
-    "name:": "Four Barrel Coffee",
-    "url:": "https://www.yelp.com/biz/four-barrel-coffee-san-francisco",
-    "latitude:":  "37.7670169511878",
-    "longitude:": "-122.42184275",
-    "city:": "San Francisco",
-    "country:": "US",
-    "state:": "CA",
-    "address:": "375 Valencia St",
-    "zip_code:": "94113",
-    "distance:": "1604.23",
-    "transactions:": ['pickup', 'delivery']
+    "name": "Four Barrel Coffee",
+    "url": "https://www.yelp.com/biz/four-barrel-coffee-san-francisco",
+    "latitude": 37.7670169511878,
+    "longitude": -122.42184275,
+    "city": "San Francisco",
+    "country": "US",
+    "state": "CA",
+    "address": "375 Valencia St",
+    "zip_code": "94113",
+    "distance": 1604.23,
+    "transactions": ["pickup", "delivery"]
 }
 
+
 def explore_data():
-    print(restaurant_1 ["url:"])
-    print(restaurant_1 ["latitude:"], restaurant_1 ["longitude:"])
-    print("375 Valencia St, San Francisco, CA, 94113") # Not sure if this 
-    # is the way I was intended to do it but no other way worked that I  tried. 
-    #if statement, str, "," between each key value pair, none worked
-    
+    print(restaurant_1 ["url"])
+    print(restaurant_1 ["latitude"], restaurant_1 ["longitude"])
+    print(f'{restaurant_1 ["address"]}, {restaurant_1 ["city"]}, {restaurant_1 ["state"]}, {restaurant_1 ["zip_code"]}')
+        
     # 1.1 TODO: Print the URL of the website of Four Barrel Coffee.
 
     # 1.2 TODO: Print the latitude and longitude of Four Barrel Coffee, using one print statement.
@@ -34,17 +33,8 @@ def explore_data():
 
 # explore_data()
 
+
 def favorite_restaurant():
-    favorite_restaurant = {'name': 'Islands', 'address': '10848 W Pico Blvd', 
-                           'favorite_dish': 'Tortilla Soup'
-    }
-              
-    print(favorite_restaurant)
-    del favorite_restaurant["favorite_dish"]
-    print(favorite_restaurant)
-    favorite_restaurant["address"] = "116th & Broadway, NY 10016"
-    print(favorite_restaurant["address"])
-    
     # Let's ask the user for some information about their favorite restaurant
 
     # 2.1 TODO: Create an empty dictionary in a variable called `favorite_restaurant`
@@ -56,7 +46,22 @@ def favorite_restaurant():
     #        "address" : "116th & Broadway, NY 10016",
     #        "favorite_dish" : "Chicken BLT Sandwich"
     #    }
-
+    favorite_restaurant = {}
+    
+    for f in range(1):
+        fav_rs_name = input("What is the name of your favorite restaurant? ")
+        fav_rs_address = input("What is the address of your favorite restaurant? ")
+        fav_rs_dish = input("What is your favorite dish at your favorite restaurant? ")
+        
+    favorite_restaurant["name"] = fav_rs_name
+    favorite_restaurant["address"] = fav_rs_address
+    favorite_restaurant["favorite_dish"] = fav_rs_dish
+    print(favorite_restaurant)
+    del favorite_restaurant["favorite_dish"]
+    print(favorite_restaurant)
+    favorite_restaurant["address"] = "116th & Broadway, NY 10016"
+    print(favorite_restaurant["address"])
+    
     # 2.3 TODO: Print out your dictionary to make sure it populated correctly
 
     # Oh no, the restaurant stopped serving the user's favorite dish!
@@ -70,14 +75,22 @@ def favorite_restaurant():
 
     # 2.7 TODO: Print out the restaurant's new address by printing the dictionary's value 
     # for the key `address`
-    
+
 # favorite_restaurant()
 
 
 def clean_print():
     for key, value in restaurant_1.items():
-        print(key, value)
+        print('{}: {}'.format(key, value))
         
+        
+    # Peter's Note:
+    # I created a for loop as written above but with print(key,":",value). 
+    # My run got a space between key and colon when
+    # I printed. I was stuck on this for a while before I went to stackoverflow and learned
+    # the .format method. Empty brackets can be used as placeholders and .format returns the 
+    # keys and values as strings.
+
     # It's hard to read the contents of a dictionary when we print the whole thing out.
 
     # 3.1 TODO: Instead, loop through each item-pair in the `restaurant_1` dictionary
